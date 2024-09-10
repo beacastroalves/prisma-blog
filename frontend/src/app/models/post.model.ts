b64dec9826fec927cf8ef080e540f02ef3d073cc
Beatriz Castro <beacastro1008@gmail.com>
2024-09-10 16:04:37 +0100

    Posts Page

--- frontend/src/app/models/post.model.ts
+++ frontend/src/app/models/post.model.ts
@@ -1,5 +1,12 @@
+export interface PostUser {
+  username: string;
+}
+
 export interface Post {
+  createdAt: Date;
+  updatedAt: Date;
   id: string;
   title: string;
   description: string;
+  user: PostUser;
 }
--- frontend/src/app/pages/posts/posts.page.ts
+++ frontend/src/app/pages/posts/posts.page.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Post } from "../../models/post.model";
 
 @Component({
   selector: 'posts',
@@ -6,5 +7,78 @@ import { Component } from "@angular/core";
   styleUrls: ['./posts.page.scss'],
 })
 export class PostsPage {
-  value = 'Posts works!';
+  posts: Post[] = [
+    {
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      id: '01',
+      title: 'Title 01',
+      description: 'Description 01',
+      user: { username: 'beacastro' }
+    },
+    {
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      id: '02',
+      title: 'Title 02',
+      description: 'Description 02',
+      user: { username: 'beacastro' }
+    },
+    {
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      id: '03',
+      title: 'Title 03',
+      description: 'Description 03',
+      user: { username: 'beacastro' }
+    },
+    {
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      id: '04',
+      title: 'Title 04',
+      description: 'Description 04',
+      user: { username: 'beacastro' }
+    },
+    {
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      id: '05',
+      title: 'Title 05',
+      description: 'Description 05',
+      user: { username: 'beacastro' }
+    },
+    {
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      id: '06',
+      title: 'Title 06',
+      description: 'Description 06',
+      user: { username: 'beacastro' }
+    },
+    {
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      id: '07',
+      title: 'Title 07',
+      description: 'Description 07',
+      user: { username: 'beacastro' }
+    },
+    {
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      id: '08',
+      title: 'Title 08',
+      description: 'Description 08',
+      user: { username: 'beacastro' }
+    },
+    {
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      id: '09',
+      title: 'Title 09',
+      description: 'Description 09',
+      user: { username: 'beacastro' }
+    }
+  ];
 }
