17ec285d49219a7ee4a28950a7360f6ec1923b36
Beatriz Castro <beacastro1008@gmail.com>
2024-09-24 13:07:52 +0100

    Creating the spring boot

---
+++ backend/src/test/java/com/prismablog/backend/PrismaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prismablog.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrismaBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
