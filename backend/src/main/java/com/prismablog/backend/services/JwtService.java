5da561d36e1d43220ae5752e193de3874f77ee54
Beatriz Castro <beacastro1008@gmail.com>
2024-10-01 19:34:22 +0100

    add @Service to the JwtService class

--- backend/src/main/java/com/prismablog/backend/services/JwtService.java
+++ backend/src/main/java/com/prismablog/backend/services/JwtService.java
@@ -1,7 +1,9 @@
 package com.prismablog.backend.services;
 
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
+@Service
 public class JwtService {
   
   public String extractUsername(String token) {
