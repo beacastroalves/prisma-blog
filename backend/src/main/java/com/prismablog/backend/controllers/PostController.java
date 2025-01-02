58f33b829f17b80752b74fc9493246e13a4c02f3
Beatriz Castro <beacastro1008@gmail.com>
2025-01-02 11:36:23 +0000

    post endpoint part1

--- backend/src/main/java/com/prismablog/backend/controllers/PostController.java
+++ backend/src/main/java/com/prismablog/backend/controllers/PostController.java
@@ -1,5 +1,45 @@
 package com.prismablog.backend.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.prismablog.backend.controllers.dto.request.PostRequest;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/posts")
 public class PostController {
-  
+
+  @GetMapping
+  public ResponseEntity<?> fetAll() {
+    return ResponseEntity.ok("All posts");
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<?> fetchById(@PathVariable Long id) {
+    return ResponseEntity.ok("User ID: " + id);
+  }
+
+  @PostMapping
+  public ResponseEntity<?> store(@RequestBody PostRequest request) {
+    return ResponseEntity.ok("Creating post, title: " + request.getTitle() + ", description: " + request.getDescription());
+  }
+
+  @PutMapping("{id}")
+  public ResponseEntity<?> update(@PathVariable Long id, @RequestBody PostRequest request) {
+    return ResponseEntity.ok("Updated postId: " + id + " with the values -> title: " + request.getTitle() + ", description: " + request.getDescription());
+  }
+
+  @DeleteMapping("{id}")
+  public ResponseEntity<?> delete(@PathVariable Long id) {
+    return ResponseEntity.ok("Trying to delete post id: " + id);
+  }
 }
---
+++ backend/src/main/java/com/prismablog/backend/controllers/dto/request/PostRequest.java
@@ -0,0 +1,23 @@
+package com.prismablog.backend.controllers.dto.request;
+
+public class PostRequest {
+
+  private String title;
+  private String description;
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
